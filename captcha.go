e7df258358d8119739748ac4c20937b0bbe0ce7a
gouguoyin <245629560@qq.com>
2025-01-08 12:18:05 +0800

    refactor(captcha): 重构 captcha 包
    - 移除了多余的注释和导入信息
    - 更新了包的版权信息
    -优化了代码结构和命名

--- captcha.go
+++ captcha.go
@@ -1,24 +1,12 @@
-// Copyright 2017 Eric Zhou. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// fork from https://github.com/mojocn/base64Captcha
 
-// Package base64Captcha supports digits, numbers,alphabet, arithmetic, audio and digit-alphabet captcha.
-// base64Captcha is used for fast development of RESTful APIs, web apps and backend services in Go. give a string identifier to the package and it returns with a base64-encoding-png-string
+// Package captcha supports digits, numbers,alphabet, arithmetic, audio and digit-alphabet captcha.
+// captcha is used for fast development of restfull APIs, web apps and backend services in Go. give a string identifier to the package, and it returns with a base64-encoding-png-string
 package captcha
 
 import "strings"
 
-// Captcha captcha basic information.
+// Captcha basic information.
 type Captcha struct {
 	Driver Driver
 	Store  Store
@@ -46,7 +34,7 @@ func (c *Captcha) Generate() (id, b64s, answer string, err error) {
 
 // Verify by a given id key and remove the captcha value in store,
 // return boolean value.
-// if you has multiple captcha instances which share a same store.
+// if you have multiple captcha instances which share a same store.
 // You may want to call `store.Verify` method instead.
 func (c *Captcha) Verify(id, answer string, clear bool) (match bool) {
 	vv := c.Store.Get(id, clear)
