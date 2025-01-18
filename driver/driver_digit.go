790f79801a01e2d504bf62a054ebaf206181cc97
gouguoyin <245629560@qq.com>
2025-01-18 09:50:25 +0800

    refactor(base64Captcha): 重构数字验证码驱动
    -重构了 DriverDigit 结构体，增加了 NoiseCount、ShowLineOptions、Source 和 BgColor 字段
    - 重写了 DrawCaptcha 和 GenerateCaptcha 方法，使用了新的 DriverString 类
    - 删除了未使用的 MaxSkew 和 DotCount 字段
    - 优化了代码结构，提高了代码的可读性和可维护性

--- driver/driver_digit.go
+++ driver/driver_digit.go
@@ -1,10 +1,10 @@
 package driver
 
 import (
-	"math/rand/v2"
+	"image/color"
 )
 
-// DriverDigit config for captcha-engine-digit.
+// DriverDigit config for digit driver.
 type DriverDigit struct {
 	// Width Captcha png width in pixel.
 	Width int
@@ -12,78 +12,74 @@ type DriverDigit struct {
 	// Height png height in pixel.
 	Height int
 
-	// DefaultLen Default number of digits in captcha solution.
+	// Length random string length.
 	Length int
 
-	// DotCount Number of background circles.
-	DotCount int
+	// NoiseCount text noise count.
+	NoiseCount int
 
-	// MaxSkew max absolute skew factor of a single digit.
-	MaxSkew float64
+	// ShowLineOptions := OptionShowHollowLine | OptionShowSlimeLine | OptionShowSineLine .
+	ShowLineOptions int
+
+	// Source is a Unicode which is the rand string from.
+	Source string
+
+	// BgColor captcha image background color (optional)
+	BgColor *color.RGBA
 }
 
-// NewDriverDigit creates a driver of digit
+// NewDriverDigit creates a digit driver.
 func NewDriverDigit(d DriverDigit) *DriverDigit {
 	return mergeDriverDigit(d)
 }
 
-// DrawCaptcha creates digit captcha item
+// DrawCaptcha draws captcha item for digit driver.
 func (d *DriverDigit) DrawCaptcha(content string) (item Item, err error) {
-	// Initialize PRNG.
-	itemDigit := NewItemDigit(d.Width, d.Height, d.DotCount, d.MaxSkew)
-	// parse digits to string
-	digits := string2digits(content)
-
-	itemDigit.calculateSizes(d.Width, d.Height, len(digits))
-	// Randomly position captcha inside the image.
-	maxX := d.Width - (itemDigit.width+itemDigit.dotSize)*len(digits) - itemDigit.dotSize
-	maxY := d.Height - itemDigit.height - itemDigit.dotSize*2
-	var border int
-	if d.Width > d.Height {
-		border = d.Height / 5
-	} else {
-		border = d.Width / 5
-	}
-	x := RandomInt(maxX-border*2) + border
-	y := RandomInt(maxY-border*2) + border
-	// Draw digits.
-	for _, n := range digits {
-		itemDigit.drawDigit(digitFont[n], x, y)
-		x += itemDigit.width + itemDigit.dotSize
-	}
-	// Draw strike-through line.
-	itemDigit.strikeThrough()
-	// Apply wave distortion.
-	itemDigit.distort(rand.Float64()*(10-5)+5, rand.Float64()*(200-100)+100)
-	// Fill image with random circles.
-	itemDigit.fillWithCircles(d.DotCount, itemDigit.dotSize)
-	return itemDigit, nil
+	driverString := NewDriverString(DriverString{
+		Width:           d.Width,
+		Height:          d.Height,
+		Length:          d.Length,
+		NoiseCount:      d.NoiseCount,
+		ShowLineOptions: d.ShowLineOptions,
+		Source:          d.Source,
+		BgColor:         d.BgColor,
+	})
+	return driverString.DrawCaptcha(content)
 }
 
-// GenerateCaptcha creates captcha content and answer
-func (d *DriverDigit) GenerateCaptcha() (id, q, a string) {
-	id = RandomString()
-	digits := RandomDigits(d.Length)
-	a = digits2String(digits)
-	return id, a, a
+// GenerateCaptcha generates id, content and answer for digit driver.
+func (d *DriverDigit) GenerateCaptcha() (id, content, answer string) {
+	driverString := NewDriverString(DriverString{
+		Width:           d.Width,
+		Height:          d.Height,
+		Length:          d.Length,
+		NoiseCount:      d.NoiseCount,
+		ShowLineOptions: d.ShowLineOptions,
+		Source:          d.Source,
+		BgColor:         d.BgColor,
+	})
+	return driverString.GenerateCaptcha()
 }
 
-// mergeDriverAudio merges default driver with given digit driver
+// mergeDriverDigit merges default driver with given digit driver.
 func mergeDriverDigit(d DriverDigit) *DriverDigit {
 	if d.Height == 0 {
-		d.Height = DefaultDriverDigit.Height
+		d.Height = DefaultDriverString.Height
 	}
 	if d.Width == 0 {
-		d.Width = DefaultDriverDigit.Width
+		d.Width = DefaultDriverString.Width
+	}
+	if d.ShowLineOptions == 0 {
+		d.ShowLineOptions = DefaultDriverString.ShowLineOptions
+	}
+	if d.NoiseCount == 0 {
+		d.NoiseCount = DefaultDriverString.NoiseCount
 	}
 	if d.Length == 0 {
-		d.Length = DefaultDriverDigit.Length
+		d.Length = DefaultDriverString.Length
 	}
-	if d.MaxSkew == 0 {
-		d.MaxSkew = DefaultDriverDigit.MaxSkew
-	}
-	if d.DotCount == 0 {
-		d.DotCount = DefaultDriverDigit.DotCount
+	if d.Source == "" {
+		d.Source = TxtNumbers
 	}
 	return &d
 }
