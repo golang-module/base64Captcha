16de6fe1e1fe0f32519baa29d7e182f1a649f8dc
gouguoyin <245629560@qq.com>
2025-01-18 09:57:26 +0800

    refactor(base64Captcha): 移除默认驱动中的 BgColor 属性
    - 从所有默认驱动配置中移除了 BgColor 属性

--- driver/driver_default.go
+++ driver/driver_default.go
@@ -1,7 +1,5 @@
 package driver
 
-import "image/color"
-
 // DefaultDriverAudio is a default audio driver
 var DefaultDriverAudio = &DriverAudio{
 	Length:   6,
@@ -14,15 +12,15 @@ var DefaultDriverMath = &DriverMath{
 	Height:          32,
 	ShowLineOptions: 0,
 	NoiseCount:      0,
-	BgColor:         &color.RGBA{R: 0, G: 0, B: 0, A: 0},
 }
 
 // DefaultDriverDigit is a default digit driver
 var DefaultDriverDigit = &DriverDigit{
-	Width:   100,
-	Height:  32,
-	Length:  6,
-	BgColor: &color.RGBA{R: 0, G: 0, B: 0, A: 0},
+	Width:           100,
+	Height:          32,
+	Length:          6,
+	ShowLineOptions: 0,
+	NoiseCount:      0,
 }
 
 // DefaultDriverLetter is a default letter driver
@@ -33,7 +31,6 @@ var DefaultDriverLetter = &DriverLetter{
 	ShowLineOptions: 1,
 	NoiseCount:      0,
 	Source:          TxtAlphabet,
-	BgColor:         &color.RGBA{R: 0, G: 0, B: 0, A: 0},
 }
 
 // DefaultDriverString is a default string driver
@@ -44,7 +41,6 @@ var DefaultDriverString = &DriverString{
 	ShowLineOptions: 0,
 	NoiseCount:      0,
 	Source:          TxtNumbers + TxtAlphabet,
-	BgColor:         &color.RGBA{R: 0, G: 0, B: 0, A: 0},
 }
 
 // DefaultDriverChinese is a default chinese driver
@@ -56,5 +52,4 @@ var DefaultDriverChinese = &DriverChinese{
 	NoiseCount:      0,
 	Source:          "县果栋容他锹射纳堤洲冶架缓飞善挑捏绒既寨剧缝辆语愉谱鸟详坛饶碰扛笔试晶巴呀塘有谣辜确丝活将宪染淋范殖",
 	Fonts:           []string{"wqy-microhei.ttc"},
-	BgColor:         &color.RGBA{R: 0, G: 0, B: 0, A: 0},
 }
